92e2f245ff804042f4c8aa25619f4b7d18c40e6a
Sebastian Petak <sebastianpetak@gmail.com>
2016-12-04 19:45:33 -0600

    Changed logical || operator back to && operator in app/api/search.js, offset is now converted to a number in app/routes/index.js and in app/api/search.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -19,7 +19,7 @@ module.exports = function(app, auth, dbUrl) {
 	app.get('/:SEARCHTERMS', function(req, res) {
 		var searchTerms = req.params.SEARCHTERMS;
 		// offset used to paginate through responses
-		var offset = req.query.offset || 0
+		var offset = Number(req.query.offset) || 0
 		if (searchTerms != 'favicon.ico') {
 			search(searchTerms, offset, auth, dbUrl, function(response) {
 				console.log("Search response sent");
